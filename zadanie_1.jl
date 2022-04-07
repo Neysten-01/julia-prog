0e0018173ddd2d05c8e7cec311eda7712e28b02d
Timur Acha <74928395+Neysten-01@users.noreply.github.com>
2022-04-08 01:45:33 +0300

    Update zadanie_1.jl

--- zadanie_1.jl
+++ zadanie_1.jl
@@ -3,20 +3,26 @@
 РЕЗУЛЬТАТ: Робот — в исходном положении в центре прямого креста из маркеров, расставленных вплоть до внешней рамки.
 """
 
-function mark_kross!(r::Robot)
-    for side in (HorizonSide(i) for i=0:3) 
-        putmarkers!(r,side)
-        move_by_markers(r,inverse(side))
+function krest!(r::Robot)
+    for side in (Nord,West,Sud,Ost)
+    move_to_GorMarker!(r,side)
+    move_to_Gor!(r,inverse(side))
+    
+    
     end
-    putmarker!(r)
 end
-putmarkers!(r::Robot,side::HorizonSide) = 
-while isborder(r,side)==false 
-    move!(r,side)
-    putmarker!(r)
+    
+function move_to_GorMarker!(r::Robot,side::HorizonSide)
+    while !isborder(r,side)
+        move!(r,side)
+        putmarker!(r)
+    end
 end
-move_by_markers(r::Robot,side::HorizonSide) = 
-while ismarker(r)==true 
-    move!(r,side) 
+    
+function move_to_Gor!(r::Robot,side::HorizonSide)
+    while ismarker(r) == true
+        move!(r,side)
+    end
 end
+    
 inverse(side::HorizonSide) = HorizonSide(mod(Int(side)+2, 4))
